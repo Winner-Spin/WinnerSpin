a6e7bbf73b566bbd177ba7dd84933c8d74112a96
gunes1hakan <gunes1.hakan1@gmail.com>
2026-03-16 23:50:24 +0300

    WSPIN-27 A Firebase project was created and integrated into the application

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'login_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  print("🔥 FIREBASE BAĞLANTISI BAŞARILI! 🔥"); // Connection check
   runApp(const MyApp());
 }
 
